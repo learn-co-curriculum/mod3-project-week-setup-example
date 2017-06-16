8272dbfcde33503adbb22bb0dbc731233527fad6
curiositypaths <curiositypaths@users.noreply.github.com>
2017-06-16 09:30:41 -0400

    Add support for cross-origin resource sharing

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,5 +29,16 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rack cors configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        # Defines the urls from which to allow ajax request
+        origins '*'
+        # Defines which resources and http verbs are allowed
+        # Documentation for the options verb: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
+      end
+    end
   end
 end
