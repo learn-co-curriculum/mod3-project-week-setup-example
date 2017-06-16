31df227127553722330eb0d98bdf934d4eb3454a
curiositypaths <curiositypaths@users.noreply.github.com>
2017-06-16 14:06:53 -0400

    Remove unnecessary http verbs from the cors config and rails routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :notes, except: [:new,:edit]
+      resources :notes, only: [:index,:create]
     end
   end
 end
